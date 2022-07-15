8c9fbb3318fc99af3fdddf89d85718ca5a492e52
Christabelle143 <christabelleyouta143@gmail.com>
2022-07-15 20:42:36 +0100

    create component for addbook, categories and navbar

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,1 @@
-import React from 'react';
-/* eslint-disable react/prop-types */
-/* eslint-disable  react/destructuring-assignment */
 
-function Book(props) {
-  return (
-    <div className="book-container">
-      <h3 className="book-title">{props.title}</h3>
-      <p className="book-author">{props.author}</p>
-      <button type="button" className="remove-btn">Remove</button>
-    </div>
-  );
-}
-export default Book;
