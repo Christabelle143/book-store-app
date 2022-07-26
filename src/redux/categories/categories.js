ebe36e2e707e36c61b08bf969f40860b93140fab
Christabelle143 <christabelleyouta143@gmail.com>
2022-07-26 16:44:17 +0100

    Add book's actions and reducers

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,20 @@
+// Action type
+const STATUS_CHECK = 'bookstore/categories/STATUS_CHECK';
+const initialState = [];
+
+// Action creator
+export const check = () => ({
+  type: STATUS_CHECK,
+});
+
+// Reducer
+const categoriesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STATUS_CHECK:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
+
+export default categoriesReducer;
