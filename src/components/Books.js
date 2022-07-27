80c5519bc199e6a709741e28e14cf0cc3b8ffced
Christabelle143 <christabelleyouta143@gmail.com>
2022-07-27 12:36:56 +0100

    fix linters errors

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Book from './Book';
 import AddBook from './AddBook';
+/* eslint-disable react/prop-types */
+/* eslint-disable  react/destructuring-assignment */
 
 const Books = () => {
   const bookList = [{
